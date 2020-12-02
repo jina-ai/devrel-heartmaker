2a865db903f831e538980c1cfb8e0b32a7c69138
Alex Cureton-Griffiths <alexcg@outlook.com>
2020-12-02 16:18:36 +0100

    feat: integrate getting contributors

--- README.md
+++ README.md
@@ -1,30 +1,28 @@
 # Heart Maker
 
+A script by [Jina AI](https://github.com/jina-ai/jina/) to show love for our GitHub contributors
+
 <p align="center">
   <a href="https://github.com/jina-ai/jina"><img align="center" src="example.png"></a>
 </p>
 
 This script:
-- scrapes a list of contributor avatar URLs from a `README.md` file ([example](https://github.com/jina-ai/jina/#contributors-))
+- scrapes a list of contributor avatar images from all the repositories of a GitHub organization
 - creates a mosaic of the images in the shape of a heart
-- presents the heart and allows user to save as image
+- presents the heart and allow saving as image
 
 ## Usage
 
-1. Ensure you've used [all-contributors](https://github.com/all-contributors/all-contributors) to get contributors into your repo's `README.md`
-2. Clone this repo: 
-3. `cd devrel-heartmaker`
-4. Save your `README.md` into `input`
-5. `pip install -r requirements.txt`
-6. `python app.py`
-7. The script will take a while (the more contributors, the more time)
-8. A window will pop up allowing you to view and save the image
+1. [Generate a new GitHub token](https://github.com/settings/tokens). Ensure **read:org** and **read:user** are checked
+2. Export the token as an environment variable: `export GITHUB_TOKEN=<whatever_your_token_is>`
+3. Clone this repo: `git clone git@github.com:jina-ai/devrel-heartmaker.git`
+4. `cd devrel-heartmaker`
+6. `pip install -r requirements.txt`
+7. `python app.py --org_name=<your_org_name>`
+8. The script will take a while (the more contributors, the more time)
+9. A window will pop up allowing you to view and save the heart mosaic
 
 ## FAQ
-
-### Why can't the script can't see my contributors?
-
-Edit `regex1` and `regex2` in `app.py`
 
 ### How can I change the size of the avatars/heart?
 
@@ -34,9 +32,15 @@ Tweak `grid_size` in `app.py`
 
 There are only a finite number of ways to fill a heart. You couldn't do it with just 1 or 2 avatars without duplication after all. As long as there are gaps in the heart, they have to be filled somehow, and we do this via duplication
 
-### I have too many contributors, can't show them all
+### I have too many contributors, and I can't show them all
 
 Change `grid_size = 20` to a larger number, say `grid_size = 30` (~ 150 contributors), `grid_size = 40` (~ 200 contributors) 
+
+## What Can You Do With It?
+
+### Personalized Thanksgiving Social Banners
+
+![](thanksgiving.png)
 
 ## Inspiration
 
--- app.py
+++ app.py
@@ -8,57 +8,85 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 from mpl_toolkits.axes_grid1 import ImageGrid
+import requests
+import os
+from github import Github
 
-input_file = 'input/README.md'
-
+# Key settings
 grid_size = 20
 avatar_size = 64
-regex1 = r'<!-- markdownlint-disable -->(.*)<!-- markdownlint-restore -->'
-regex2 = r"<img src=\"(.*?)\" class=\"avatar-user\""
 
-# input_file = 'README.md'
+# Get Contributors start
 
-print(f"Getting contributors from {input_file}")
-with open(input_file) as fp:
-    r = fp.read()
-    m1 = list(re.finditer(regex1, r, re.DOTALL))[0].group()
-    m2 = re.finditer(regex2, r)
-    all_contributors = [mm.groups()[0] for mm in m2]
+@click.command()
+@click.option("--org", help="Name of your GitHub organization", required=True)
+@click.option(
+    "--token", help="GitHub token", default=os.environ["GITHUB_TOKEN"], required=True
+)
+def make_heart(org, token):
+    g = Github(token)
+    org = g.get_organization(org)
 
-fig = plt.figure(figsize=(4., 4.))
-grid = ImageGrid(fig, 111,  # similar to subplot(111)
-                nrows_ncols=(grid_size, grid_size),  # creates 2x2 grid of axes
-                axes_pad=0.02,  # pad between axes in inch.
-                share_all=True)
+    repos = []
+    contrib_counter = 0
 
-grid[0].get_yaxis().set_ticks([])
-grid[0].get_xaxis().set_ticks([])
+    all_repos = org.get_repos()
 
-print("Plotting heart (this may take a while)")
-for id, ax in enumerate(grid):
-    y, x = grid_size / 2 - int(id / grid_size), - grid_size / 2 + int(id % grid_size)
-    x /= (grid_size / 3)
-    y /= (grid_size / 3)
-    # print(f'x: {x} y: {y}')
-    ax.axis('off')
-    if (x ** 2 + y ** 2 - 1) ** 3 - (x ** 2) * (y ** 3) < 0 and all_contributors:
-        shuffle(all_contributors)
-        url = all_contributors.pop(0)
-        if isinstance(url, str):
-            req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-            # Iterating over the grid returns the Axes.
-            with urlopen(req) as f:
-                img = Image.open(f)
-                img = img.resize((avatar_size, avatar_size))
-                im = np.asarray(img)
-        all_contributors.append(url)
-        ax.imshow(im)
+    for repo in all_repos:
+        repos.append(str(repo.full_name))
 
-    else:
-        # print('reject')
-        pass
+    print(f"Getting contributors from {org.name}'s {len(repos)} repositories")
+
+    all_contributors = []
+    for repo in repos:
+        repo = g.get_repo(repo)
+        contribs = repo.get_contributors()
+        for user in contribs:
+            if not user.avatar_url in all_contributors:
+                all_contributors.append(user.avatar_url)
+    # Get Contributors end
+
+    # Render heart start
+    fig = plt.figure(figsize=(4.0, 4.0))
+    grid = ImageGrid(
+        fig,
+        111,  # similar to subplot(111)
+        nrows_ncols=(grid_size, grid_size),  # creates 2x2 grid of axes
+        axes_pad=0.02,  # pad between axes in inch.
+        share_all=True,
+    )
+
+    grid[0].get_yaxis().set_ticks([])
+    grid[0].get_xaxis().set_ticks([])
+
+    print("Plotting heart (this may take a while)")
+    for id, ax in enumerate(grid):
+        y, x = grid_size / 2 - int(id / grid_size), -grid_size / 2 + int(id % grid_size)
+        x /= grid_size / 3
+        y /= grid_size / 3
+        ax.axis("off")
+        if (x ** 2 + y ** 2 - 1) ** 3 - (x ** 2) * (y ** 3) < 0 and all_contributors:
+            shuffle(all_contributors)
+            url = all_contributors.pop(0)
+            if isinstance(url, str):
+                req = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+                # Iterating over the grid returns the Axes.
+                with urlopen(req) as f:
+                    img = Image.open(f)
+                    img = img.resize((avatar_size, avatar_size))
+                    im = np.asarray(img)
+            all_contributors.append(url)
+            ax.imshow(im)
+
+        else:
+            pass
+
+    plt.box(False)
+    plt.axis("off")
+    plt.show()
+
+    # Render heart end
 
 
-plt.box(False)
-plt.axis('off')
-plt.show()
+if __name__ == "__main__":
+    make_heart()
